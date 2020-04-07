3b82a95a91221ccad1163accdfc75d9478b21445
Calvin <calvin.leon19@gmail.com>
2020-04-07 10:33:00 +0700

    TableView

---
+++ 7 Days Challenge/Challenge.swift
@@ -0,0 +1,15 @@
+//
+//  Challenge.swift
+//  7 Days Challenge
+//
+//  Created by Calvin Leonardo on 07/04/20.
+//  Copyright © 2020 Mini Challenge 1 - G19. All rights reserved.
+//
+
+import Foundation
+
+struct Challenge {
+    var challengeName: String
+    var challengeDay: Int
+    var challengeScore: Int
+}
--- 7 Days Challenge/HomeViewController.swift
+++ 7 Days Challenge/HomeViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITableViewController {
 
+    let challenges = [
+        Challenge(challengeName: "Push Up", challengeDay: 1, challengeScore: 150),
+        Challenge(challengeName: "Sit Up", challengeDay: 2, challengeScore: 150),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return challenges.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "challengeCell", for: indexPath)
+        let challenge = challenges[indexPath.row]
+        
+        cell.textLabel?.text = challenge.challengeName
+        cell.detailTextLabel?.text = "\(challenge.challengeScore)"
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
     }
     
 
