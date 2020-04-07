10a6cbf0b1879b5b5d66df8d2b1ae67bce4c72dc
Theofani Samuel <liow.ts@gmail.com>
2020-04-07 12:36:01 +0700

    add LaunchScreen view swift file

---
+++ 7 Days Challenge/LaunchScreenView.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenView.swift
+//  7 Days Challenge
+//
+//  Created by Theofani on 07/04/20.
+//  Copyright © 2020 Mini Challenge 1 - G19. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
