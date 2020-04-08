ee6bcbd9cc4a63702ebab734093141a7f297ec7c
michael alinskie <alinskie@gmail.com>
2020-04-08 10:15:11 +0700

    masukin json file achievement modal

--- 7 Days Challenge/AchievementModal.swift
+++ 7 Days Challenge/AchievementModal.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import Lottie
 
 class AchievementModal: UIViewController {
 
+    
+     let animationView = AnimationView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+               setupAnimation()
+           }
 
+           private func setupAnimation(){
+               animationView.animation = Animation.named("reward")
+               animationView.frame = view.bounds
+               animationView.backgroundColor = .white
+               animationView.contentMode = .scaleAspectFit
+               animationView.loopMode = .loop
+               animationView.play()
+               view.addSubview(animationView)
+           }
         // Do any additional setup after loading the view.
     }
     
@@ -27,4 +42,5 @@ class AchievementModal: UIViewController {
     }
     */
 
-}
+
+
