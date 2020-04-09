b0bb9ae781e11b444ea4425109b3f8daea2c84c1
Calvin <calvin.leon19@gmail.com>
2020-04-09 09:20:51 +0700

    ADD VideoTableViewCell, DELETE ChallengeView

---
+++ 7 Days Challenge/Controller/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  7 Days Challenge
+//
+//  Created by Calvin Leonardo on 09/04/20.
+//  Copyright © 2020 Mini Challenge 1 - G19. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
