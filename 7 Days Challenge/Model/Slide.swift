085be287db590d3c9195bdd09ac3ad5d8a8ded2c
Anthony Angkasa <anthony.angkasa1@gmail.com>
2020-04-09 10:00:32 +0700

    Onboarding Screen

---
+++ 7 Days Challenge/Model/Slide.swift
@@ -0,0 +1,23 @@
+//
+//  Slide.swift
+//  7 Days Challenge
+//
+//  Created by Anthony Angkasa on 09/04/20.
+//  Copyright © 2020 Mini Challenge 1 - G19. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelDescription: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
