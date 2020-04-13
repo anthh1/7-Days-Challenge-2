ce037737681d461bbe79e08ce4a80bb893b6cae9
Theofani Samuel <liow.ts@gmail.com>
2020-04-13 18:29:10 +0700

    Notification feature

---
+++ 7 Days Challenge/Controller/ChallengeTableViewCell 2.swift
@@ -0,0 +1,33 @@
+//
+//  ChallengeTableViewCell.swift
+//  7 Days Challenge
+//
+//  Created by Calvin Leonardo on 08/04/20.
+//  Copyright © 2020 Mini Challenge 1 - G19. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var challengeImageBg: UIImageView!
+    @IBOutlet weak var challengeDayLbl: UILabel!
+    @IBOutlet weak var challengeNameLbl: UILabel!
+    @IBOutlet weak var challengeScoreLbl: UILabel!
+    @IBOutlet weak var bgTable: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        challengeNameLbl.numberOfLines = 2
+        bgTable.layer.cornerRadius = 30
+        self.selectedBackgroundView = UIView()
+//      self.selectionStyle = .none
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 25, bottom: 10, right: 25))
+    }
+}
--- 7 Days Challenge/Controller/HomeViewController.swift
+++ 7 Days Challenge/Controller/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 class HomeViewController: UITableViewController {
 
@@ -34,6 +35,42 @@ class HomeViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //-- LOCAL NOTIFICATIONS FEATURE --//
+        
+        //Getting user permission
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound])
+        {
+            (granted, error) in
+        }
+        
+        //Set notification content
+        let content = UNMutableNotificationContent()
+        content.title = "7Days Challenge"
+        content.body = "Hey, let's do a challenge!"
+        
+        //Create notification trigger
+        let date = Date().addingTimeInterval(5)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        //Create the request
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        //Register to notification center
+        center.add(request) {
+            (error) in
+            //check the error parameter and handle any errors
+        }
+        
+        //-- Notifications feature codes end here --//
+        
+        
         
         self.navigationItem.setHidesBackButton(true, animated: true);
         self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
